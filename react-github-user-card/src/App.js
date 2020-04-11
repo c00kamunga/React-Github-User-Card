f97c702f61c89db20ee257f1242bbfae7d0ea2fb
c00kamunga <tsothalanti2@yahoo.com>
2020-04-10 18:36:40 -0700

    added contructor, super, and internal state for App component

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 class App extends React.Component(){
-
+constructor(){
+  super();
+  this.state = {
+    user:[],
+    followers:{}
+  }
+}
   render(){
     return(
       <div>
